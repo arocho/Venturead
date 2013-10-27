9feb1c8d1217ece2849cd05ea04cac384ae5d5ed
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-10-27 14:47:41 -0400

    Hash testing function for 10000 randomly generated locations works!

--- src/game/ventureread/core/world/test/LocationTest.java
+++ src/game/ventureread/core/world/test/LocationTest.java
@@ -1,6 +1,11 @@
 package game.ventureread.core.world.test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
 import game.venturead.core.world.Direction;
 import game.venturead.core.world.Location;
 
@@ -10,12 +15,14 @@ import org.junit.Test;
 public class LocationTest {
 	
 	private Location house;
+	private Location houseClone;
 	private Location attic;
 	private Location garden;
 	
 	@Before
 	public void setUp() throws Exception {
 		house  = Location.newLocation("The House", "You see a quaint house, with tons of knickknacks.");
+		houseClone = Location.newLocation("The House", "This is a clone of the house, but it is equal to it.");
 		attic  = Location.newLocation("The Attic", "You see a grue!  Run!");
 		garden = Location.newLocation("The Garden", "You see a beautiful colorful garden.");
 		
@@ -33,17 +40,47 @@ public class LocationTest {
 	@Test
 	public void testHashCode() {
 		
-		fail("Not yet implemented");
-	}
+		assertEquals("Houses with equal names should produce the same hash", house.hashCode(), houseClone.hashCode());
+		
+		Random r = new Random(System.currentTimeMillis()+System.nanoTime()); 
+		RandomStringUtils.random(7, true, false);
+		String[] randomNames = new String[10000];
+		
+		System.out.println("Test");
+		
+		for(int i=0; i<10000; i++) {
+			randomNames[i] = RandomStringUtils.random(7, true, false);
+		}
 
-	@Test
-	public void testNewLocation() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testConnectToLocation() {
-		fail("Not yet implemented");
+		
+		Location location1;
+		Location location2;
+		int maxTimesLocationHashClashed = 0;
+		
+		for(int i=0; i<10000; i++) {
+			String nameToCheck = randomNames[i];
+			location1 = Location.newLocation(nameToCheck, "This is a mock description.");
+			int numTimesLocationHashClashed = 0; 
+				//I expect this to be one because it will always clash with itself.
+			
+			for(String otherNameToCheck : randomNames) {
+				location2 = Location.newLocation(otherNameToCheck, "This is another mock description.");
+				if(location1.hashCode() == location2.hashCode()) {
+					numTimesLocationHashClashed++;
+					if(numTimesLocationHashClashed > 1)
+					{
+						System.out.print("Location "+location1.getName());
+						System.out.println(" clashed with "+location2.getName()+" more than once.");
+					}
+				}
+			}
+			
+			if(numTimesLocationHashClashed > maxTimesLocationHashClashed) {
+				maxTimesLocationHashClashed = numTimesLocationHashClashed;
+			}
+		}
+		
+		assertEquals(1, maxTimesLocationHashClashed);
 	}
 
 	@Test
