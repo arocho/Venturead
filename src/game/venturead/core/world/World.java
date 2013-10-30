727039c1ae20e3f8bf45cff2406cb241457f531f
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-10-30 01:53:58 -0400

    Implemented the Path inner class, which represents a path through the world.  Includes an iterator, and auxiliary methods for quick access to necessary elements of the Path.  A path also contains a cost, which represents the game's "cost" for traversing the entire thing.  For now, this cost is assumed to be the length of the Path itself (but is still computed independently elsewhere).

--- src/game/venturead/core/world/World.java
+++ src/game/venturead/core/world/World.java
@@ -95,4 +95,144 @@ public class World
 		return sb.toString();
 	}
 
+	public class Path<T> implements Iterable<T>, Comparable<T>
+	{
+		private LinkedList<T> pathList;
+		private int pathCost;
+
+		public Path() {
+			this.pathList = new LinkedList<T>();
+			this.pathCost = 0;
+		}
+
+		public Path(T init, int pathCost) {
+			this.pathList = new LinkedList<T>();
+			this.pathList.add(init);
+			this.pathCost = 0;
+		}
+
+		public Path(LinkedList<T> existingPathList, int pathCost) {
+			this.pathList = existingPathList;
+			this.pathCost = pathCost;
+		}
+
+		public Path(Path<T> existingPath, int pathCost) {
+			this.pathList = existingPath.pathList;
+			this.pathCost = pathCost;
+		}
+
+		/**
+		 * Appends the item to this <tt>Path</tt>, and returns the list with this item.
+		 * @param item the item to add.
+		 * @return the <tt>Path</tt> after appending
+		 */
+		public Path<T> append(T item) {
+			this.pathList.add(item);
+			return this;
+		}
+
+		/**
+		 * @return the last item of this <tt>Path</tt>
+		 */
+		public T getLastItem() {
+			return this.pathList.get(this.pathList.size()-1);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			//two Paths are equal if both their path costs, and
+			//their contents are equal
+
+			if(obj == null)
+				return false;
+
+			else if(!(obj instanceof Path<?>))
+				return false;
+
+			else {
+				Path<?> other = (Path<?>) obj;
+				if(this.pathCost != other.pathCost)
+					return false;
+
+				if(!this.pathList.equals(other.pathList))
+					return false;
+			}
+
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = 11 * this.pathList.peekLast().hashCode();
+			return hash;
+		}
+
+		@Override
+		public int compareTo(Object arg0) {
+
+			if(arg0 == null)
+				throw new NullPointerException(arg0 + " cannot be null");
+
+			else if(!(arg0 instanceof Path<?>))
+				throw new ClassCastException(arg0.getClass().getName() +" is not of type "+this.getClass().getName());
+
+			Path<?> other = (Path<?>) arg0;
+
+			if(this.pathCost < other.pathCost)
+				return -1;
+
+			else if(this.pathCost > other.pathCost)
+				return 1;
+
+			else
+				return 0;
+		}
+
+		@Override
+		public Iterator<T> iterator() {
+			return new PathIterator<T>(this);
+		}
+
+		/**
+		 * The PathIterator that makes <tt>World.Path</tt> iterable.
+		 * @param <S> the PathIterator type.
+		 * @see <tt>World.Path<T></tt> at <tt>World.java</tt> 
+		 */
+		private class PathIterator<S> implements Iterator<S> {
+			private int iteratorIndex;
+			private int iterablePathSize;
+			private Path<S> iterablePath;
+
+			public PathIterator(Path<S> p) {
+				iteratorIndex = 0;
+				iterablePathSize = p.pathList.size();
+			}
+
+			@Override
+			public boolean hasNext() {
+				if(iterablePathSize > 0 || iteratorIndex < iterablePathSize-1)
+					return true;
+
+				return false;
+			}
+
+			@Override
+			public S next() {
+				S pathItem = iterablePath.pathList.get(iteratorIndex);
+				iteratorIndex++;
+				return pathItem;
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException("The PathIterator does not support this method.");
+
+			}
+
+		}
+
+
+
+	}
+
 }
