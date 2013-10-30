a93be6cdcee1fb91b8b1e3bda93b6704da69ca48
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-10-29 20:19:16 -0400

    Added auxiliary method to return the opposite direction of a parameter one.

--- src/game/venturead/core/world/Direction.java
+++ src/game/venturead/core/world/Direction.java
@@ -8,5 +8,50 @@ public enum Direction {
 	NORTH, SOUTH, EAST, WEST, 
 	NORTHWEST, NORTHEAST,
 	SOUTHWEST, SOUTHEAST,
-	UP, DOWN, LEFT, RIGHT
+	UP, DOWN, LEFT, RIGHT;
+	
+	public static Direction findOppositeDirection(Direction direction) {
+
+		switch(direction)
+		{
+			case NORTH:
+				return Direction.SOUTH;
+				
+			case SOUTH:
+				return Direction.NORTH;
+				
+			case EAST:
+				return Direction.WEST;
+				
+			case WEST:
+				return Direction.EAST;
+			
+			case UP:
+				return Direction.DOWN;
+				
+			case DOWN:
+				return Direction.UP;
+				
+			case LEFT:
+				return Direction.RIGHT;
+				
+			case RIGHT:
+				return Direction.LEFT;
+				
+			case NORTHEAST:
+				return Direction.SOUTHWEST;
+				
+			case NORTHWEST:
+				return Direction.SOUTHEAST;
+				
+			case SOUTHEAST:
+				return Direction.NORTHWEST;
+				
+			case SOUTHWEST:
+				return Direction.NORTHEAST;
+					
+			default:
+				throw new IllegalArgumentException("Direction " +direction.name()+" not recognized.");
+		}
+	}
 }
