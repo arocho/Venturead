fab351a45af93af4a64fc7e06059225a3a5db8be
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-10-30 01:51:36 -0400

    Basic skeleton for the Character.

--- src/game/venturead/core/characters/Character.java
+++ src/game/venturead/core/characters/Character.java
@@ -1,5 +1,18 @@
 package game.venturead.core.characters;
 
-public class Character {
+import game.ventureread.core.items.Item;
 
+import java.util.HashMap;
+
+public abstract class Character 
+{
+	private String name;
+	private double healthPoints;
+	private HashMap<Item,Integer> items;
+		//item, quantity held
+	
+	
+	
+	
+	
 }
