6d110fe0fda654612b6746f47306ac52c73a1922
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-10-29 20:48:54 -0400

    Added hashCode/equals methods

--- src/game/venturead/core/world/Path.java
+++ src/game/venturead/core/world/Path.java
@@ -6,11 +6,11 @@ package game.venturead.core.world;
  * @author recardona
  */
 public class Path {
-	
+
 	private boolean isLocked;
 	private Location source;
 	private Location destination;
-	
+
 	/**
 	 * Creates a <tt>Path</tt> from the <i>source</i> <tt>Location</tt>
 	 * to the <i>destination</i>
@@ -22,19 +22,19 @@ public class Path {
 		if(source.equals(destination)) {
 			throw new IllegalArgumentException("Locations cannot be connected to themselves.");
 		}
-		
+
 		this.source = source;
 		this.destination = destination;
 		this.isLocked = false;
 	}
-	
+
 	/**
 	 * @return the isLocked
 	 */
 	public boolean isLocked() {
 		return isLocked;
 	}
-	
+
 	/**
 	 * Toggles this <tt>Path</tt>'s lock
 	 */
@@ -55,11 +55,33 @@ public class Path {
 	protected Location getDestination() {
 		return destination;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "path to "+this.getDestination().getName();
 	}
-	
-		
+
+	@Override
+	public int hashCode() {
+		//two paths that have the same source and destination
+		//(regardless of which is which) will produce the same hashCode
+		int hash = 37 * 41 + (this.source.hashCode() * this.destination.hashCode());
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// Two Paths are equal if their connecting endpoints 
+		// (source and destination, regardless of which is which) 
+		if(obj == null) {return false;}
+		else if(!(obj instanceof Path)) {return false;}
+		else {
+			Path other = (Path) obj;
+			if(this.hashCode() != other.hashCode()) {return false;}
+		}
+
+		return true;
+	}
+
+
 }
